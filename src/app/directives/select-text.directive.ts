66b248346a957a4907a525a11ceadf96cc3130a9
Jabir K H <jabir.kh@experionglobal.com>
2018-03-26 00:35:42 +0530

    select text directive implemented

--- src/app/directives/select-text.directive.ts
+++ src/app/directives/select-text.directive.ts
@@ -1,13 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener } from '@angular/core';
 import { ElementRef} from '@angular/core';
 @Directive({
   selector: '[appSelectText]'
 })
 export class SelectTextDirective {
 
-  constructor(event: Event) { 
-  	console.log(event.target);
-  	//code to select text inside the input element
+  constructor(private element: ElementRef) {
+  }
+
+  @HostListener('click') onclick() {
+    this.element.nativeElement.select();
   }
 
 }
