d23cb4809f99d9c2b0372ab74122a79d42b2c4eb
Jabir K H <jabir.kh@experionglobal.com>
2018-03-25 20:03:39 +0530

    rating modified

--- src/app/quiz/result/result.component.ts
+++ src/app/quiz/result/result.component.ts
@@ -8,16 +8,28 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class ResultComponent implements OnInit {
 
   scoreInHundred = 0;
+  message = null;
   @Input() score: number;
   @Output() goHome: EventEmitter<void> = new EventEmitter<void>();
   constructor() { }
 
   ngOnInit() {
     this.scoreInHundred = this.score * 20;
+    this.message = this.getMessageText();
   }
 
   goToQuizHome() {
     this.goHome.emit();
+  }
+
+  private getMessageText() {
+    if (this.score <= 2) {
+      return 'Good!!';
+    } else if (this.score <= 4) {
+      return  'Very Good!!';
+    } else {
+      return 'Excellent!!';
+    }
   }
 
 }
